142fec42bbea8ab460eb27f9b3ee29687c2be75b
andfxx27 <idea.andersen@gmail.com>
2022-03-10 11:35:52 +0700

    feat: auth middleware and helper for parsing jwt

--- helper/request.go
+++ helper/request.go
@@ -3,8 +3,16 @@ package helper
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/golang-jwt/jwt"
 )
 
 func CreateJSONDecoder(r *http.Request) *json.Decoder {
 	return json.NewDecoder(r.Body)
+}
+
+func ParseJWTClaims(r *http.Request) (jwt.MapClaims, error) {
+	claims := jwt.MapClaims{}
+	err := json.Unmarshal([]byte(r.Header.Get("claims")), &claims)
+	return claims, err
 }
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,10 +1,42 @@
 package middleware
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/andfxx27/chirps-api/constant"
+	"github.com/andfxx27/chirps-api/helper"
+	"github.com/golang-jwt/jwt"
+)
 
 func JSONResponseMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Add("content-type", "application/json")
+		next.ServeHTTP(rw, r)
+	})
+}
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		tokenString := strings.Split(r.Header.Get("Authorization"), " ")[1]
+		token, err := jwt.ParseWithClaims(tokenString, jwt.MapClaims{}, func(t *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_SECRET_KEY")), nil
+		})
+		if err != nil {
+			helper.BuildJSONResponse(http.StatusUnauthorized, constant.Unauthorized, "", rw)
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			claimsJSON, _ := json.Marshal(claims)
+			r.Header.Add("claims", string(claimsJSON))
+		} else {
+			helper.BuildJSONResponse(http.StatusUnauthorized, constant.Unauthorized, "", rw)
+			return
+		}
+
 		next.ServeHTTP(rw, r)
 	})
 }
